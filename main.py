504ebbc6c8d955fb9f6a506d4833f143a7dde63a
Andrei Comanici <candrei721@gmail.com>
2024-09-07 19:10:06 +0300

    Added sensitivity slider

--- main.py
+++ main.py
@@ -19,16 +19,6 @@ root.geometry('300x200')
 root.resizable(False, False)
 root.title('Slider Demo')
 
-import tkinter as tk
-from tkinter import ttk
-
-
-# root window
-root = tk.Tk()
-root.geometry('300x200')
-root.resizable(False, False)
-root.title('Slider Demo')
-
 
 root.columnconfigure(0, weight=1)
 root.columnconfigure(1, weight=3)
@@ -99,57 +89,55 @@ value_label.grid(
     sticky='n'
 )
 
-root.mainloop()
+while 1:
+    events = get_events()
+    for event in events:
+        controller = event.user_index
+        if event.type == EVENT_CONNECTED:
+            print(f"Controller {controller} is now connected!")
 
-# while 1:
-#     events = get_events()
-#     for event in events:
-#         controller = event.user_index
-#         if event.type == EVENT_CONNECTED:
-#             print(f"Controller {controller} is now connected!")
+        elif event.type == EVENT_DISCONNECTED:
+            print(f"Controller {controller} is now disconnected!")
 
-#         elif event.type == EVENT_DISCONNECTED:
-#             print(f"Controller {controller} is now disconnected!")
+        elif event.type == EVENT_STICK_MOVED:
+            if event.stick == LEFT:
+                l_stick_pos = (int(round(sensitivity * event.x, 0)), int(round(sensitivity * event.y, 0)))
+                mouse.move(l_stick_pos[0],-1 * l_stick_pos[1])
 
-#         elif event.type == EVENT_STICK_MOVED:
-#             if event.stick == LEFT:
-#                 l_stick_pos = (int(round(sensitivity * event.x, 0)), int(round(sensitivity * event.y, 0)))
-#                 mouse.move(l_stick_pos[0],-1 * l_stick_pos[1])
+            elif event.stick == RIGHT:
+                r_stick_pos = (int(round(event.x, 0)), int(round(event.y,0)))
+                mouse.scroll(r_stick_pos[0],r_stick_pos[1])
 
-#             elif event.stick == RIGHT:
-#                 r_stick_pos = (int(round(event.x, 0)), int(round(event.y,0)))
-#                 mouse.scroll(r_stick_pos[0],r_stick_pos[1])
+        elif event.type == EVENT_TRIGGER_MOVED:
+            if event.trigger == LEFT:
+                pass                
+            elif event.trigger == RIGHT:
+                pass
 
-#         elif event.type == EVENT_TRIGGER_MOVED:
-#             if event.trigger == LEFT:
-#                 pass                
-#             elif event.trigger == RIGHT:
-#                 pass
-
-#         elif event.type == EVENT_BUTTON_PRESSED:
+        elif event.type == EVENT_BUTTON_PRESSED:
             
-#             if event.button == "A":
-#                 mouse.press(Button.left)
-#             elif event.button == "B": 
-#                 mouse.click(Button.right,1)              
-#             elif event.button == "X":
-#                 pass
-#             elif event.button == "Y":
-#                 pass
-#             elif event.button == "DPAD_LEFT":
-#                 mouse.move(-1 * sensitivity, 0)
-#             elif event.button == "DPAD_RIGHT":
-#                 mouse.move(1 * sensitivity, 0)
-#             elif event.button == "DPAD_UP":
-#                 mouse.move(0 ,-1 * sensitivity)
-#             elif event.button == "DPAD_DOWN":
-#                 mouse.move(0 ,1 * sensitivity)
+            if event.button == "A":
+                mouse.press(Button.left)
+            elif event.button == "B": 
+                mouse.click(Button.right,1)              
+            elif event.button == "X":
+                pass
+            elif event.button == "Y":
+                pass
+            elif event.button == "DPAD_LEFT":
+                mouse.move(-1 * sensitivity, 0)
+            elif event.button == "DPAD_RIGHT":
+                mouse.move(1 * sensitivity, 0)
+            elif event.button == "DPAD_UP":
+                mouse.move(0 ,-1 * sensitivity)
+            elif event.button == "DPAD_DOWN":
+                mouse.move(0 ,1 * sensitivity)
 
-#         elif event.type == EVENT_BUTTON_RELEASED:
-#             if event.button == "A":
-#                 mouse.release(Button.left)
+        elif event.type == EVENT_BUTTON_RELEASED:
+            if event.button == "A":
+                mouse.release(Button.left)
 
-    # try:          
-    #     root.update()
-    # except tk.TclError:
-    #     break
+    try:          
+        root.update()
+    except tk.TclError:
+        break
